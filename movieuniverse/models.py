734479e616a06b5cbbc3de2c76f0c201d8adeabd
= <=>
2021-03-01 14:51:06 -0800

    Fix for Postgres issues.

--- movieuniverse/models.py
+++ movieuniverse/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 AGE_OPTIONS = [
     ('7+', '7+'),
     ('13+', '13+'),
@@ -9,19 +10,19 @@ AGE_OPTIONS = [
 ]
 
 class Movie(models.Model):
-    title = models.CharField(max_length=50)
-    year = models.CharField(max_length=4)
-    age = models.CharField(max_length=4, choices=AGE_OPTIONS)
+    title = models.TextField(max_length=50)
+    year = models.TextField(max_length=4)
+    age = models.TextField(max_length=4, choices=AGE_OPTIONS)
     IMDb_rating = models.FloatField()
-    Rotten_Tomatoes_rating = models.CharField(max_length=3)
+    Rotten_Tomatoes_rating = models.TextField(max_length=3)
     netflix = models.IntegerField(help_text='1 for available, 0 for not available')
     hulu = models.IntegerField(help_text='1 for available, 0 for not available')
     prime = models.IntegerField(help_text='1 for available, 0 for not available')
     disney = models.IntegerField(help_text='1 for available, 0 for not available')
-    directors = models.CharField(max_length=50, help_text='Separate each director with commas')
-    genres = models.CharField(max_length=50, help_text='Separate each genre with commas')
-    country = models.CharField(max_length=50, help_text='Separate each country with commas')
-    language = models.CharField(max_length=50, help_text='Separate each language with commas')
+    directors = models.TextField(max_length=50, help_text='Separate each director with commas')
+    genres = models.TextField(max_length=50, help_text='Separate each genre with commas')
+    country = models.TextField(max_length=50, help_text='Separate each country with commas')
+    language = models.TextField(max_length=50, help_text='Separate each language with commas')
     runtime = models.IntegerField(name='runtime')
 
     def __str__(self) -> str:
