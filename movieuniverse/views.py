b2e630f30883cf91d033543f63419c03f0cc90fb
= <=>
2021-03-01 12:45:24 -0800

    Fix for deployment issues.

--- movieuniverse/views.py
+++ movieuniverse/views.py
@@ -7,7 +7,7 @@ import django_pandas.io as djpd
 from random import shuffle
 
 def index(request):
-    if request.session['recommendations']:
+    if 'recommednation' in request.session:
         recent_search = request.session['recommendations'][6:]
     results = Movie.objects.filter(id__in=recent_search)
     return render(request, 'movieuniverse/index.html', {
