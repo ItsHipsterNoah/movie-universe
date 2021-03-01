89163edb150fb2eff5b32a11064e0385e703c22e
= <=>
2021-03-01 11:55:53 -0800

    Fix for deployment issues.

--- movie_universe/settings.py
+++ movie_universe/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,5 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+django_heroku.settings(locals())
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
