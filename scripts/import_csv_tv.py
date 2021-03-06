f54510e533bb4d9f5cd42eacf271a7851fbaadf2
= <=>
2021-03-05 17:39:13 -0800

    Begin Phase 2

--- scripts/import_csv_tv.py
+++ scripts/import_csv_tv.py
@@ -1,18 +1,25 @@
 import csv
 from movieuniverse.models import TVShow
 
+def process_number(integer) -> int:
+    if integer == '':
+        return 0.0
+    elif '%' in integer:
+        return float(integer.replace('%', ''))
+    else:
+        return integer
 
-with open('files/movies.csv') as f:
+with open('files/tv_shows.csv') as f:
         reader = csv.reader(f)
         for row in reader:
             _, created = TVShow.objects.get_or_create(
                 title = row[1],
                 year = row[2],
                 age = row[3],
-                IMDb_rating = row[4],
-                Rotten_Tomatoes_rating = row[5],
-                netflix = row[6],
-                hulu = row[7],
-                prime = row[8],
-                disney = row[9],
+                IMDb_rating = process_number(row[4]),
+                Rotten_Tomatoes_rating = int(process_number(row[5])),
+                netflix = int(row[6]),
+                hulu = int(row[7]),
+                prime = int(row[8]),
+                disney = int(row[9]),
             )
