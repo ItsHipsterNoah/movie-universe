6d2c4f6aaa2b743adcbaadf5556f5ca9c3218a06
= <=>
2021-03-06 03:12:34 -0800

    Phase Two - TV Shows

--- movie_universe/settings.py
+++ movie_universe/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
+
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -96,9 +97,9 @@ DATABASES = {
     },
 }
 
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
